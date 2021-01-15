c7cf3cca7eb5026fd7151c8d0ff3d0eb976de14e
Emad Messiha <emadmessiha@gmail.com>
2021-01-15 13:02:54 -0500

    Create aws_cli_mfa.fish

---
+++ aws_cli_mfa.fish
@@ -0,0 +1,8 @@
+echo "Input MFA code:"
+read MFA_CODE
+function parse_keys 
+  set -xU AWS_ACCESS_KEY_ID "$argv[2]"
+  set -xU AWS_SECRET_ACCESS_KEY "$argv[4]"
+  set -xU AWS_SESSION_TOKEN "$argv[5]"
+end
+aws sts get-session-token --output="text" --serial-number "$MFA_ID" --token-code "$MFA_CODE" > session.txt && parse_keys (cat session.txt | string split \t) && rm session.txt
