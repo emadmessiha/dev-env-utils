9d4bb8dac20ac3b4aaaa38f794e6957dc2f9d2c6
Emad Messiha <emadmessiha@gmail.com>
2021-01-18 11:31:09 -0500

    fix set -e

--- aws_cli_mfa.fish
+++ aws_cli_mfa.fish
@@ -5,4 +5,7 @@ function parse_keys
   set -xU AWS_SECRET_ACCESS_KEY "$argv[4]"
   set -xU AWS_SESSION_TOKEN "$argv[5]"
 end
+set -e AWS_ACCESS_KEY_ID
+set -e AWS_SECRET_ACCESS_KEY
+set -e AWS_SESSION_TOKEN
 aws sts get-session-token --output="text" --serial-number "$MFA_ID" --token-code "$MFA_CODE" > session.txt && parse_keys (cat session.txt | string split \t) && rm session.txt
